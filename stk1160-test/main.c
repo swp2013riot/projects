11f179f535afb8ba25afbdcd94c6f3ab5c659c21
Maximilian Ferdinand Müller <m.f.mueller@fu-berlin.de>
2013-07-08 22:01:50 +0200

    example ready

--- stk1160-test/main.c
+++ stk1160-test/main.c
@@ -6,19 +6,18 @@
 #include <board.h>
 #include <stdint.h>
 #include <hwtimer.h>
-
+#include <malloc.h>
+#include <stdlib.h>
 #include <stk1160.h>
 
-/*int main(void)
-{
-    while (1)
-    {
-        puts("Hallo!\n");
-        hwtimer_wait(HWTIMER_TICKS(1000*1000));
-    }
-}*/
+#define BUFFER_SIZE (1024*1024)
+uint8_t *buffer;
+uint8_t *buf_p;
+int frame_counter;
+int packet_counter;
 
-#if 1
+void our_handler(uint8_t *data, uint16_t length);
+
 int main(void)
 {
     printf("\n\t\t\tWelcome to RIOT\n\n");
@@ -29,22 +28,48 @@ int main(void)
     stk1160_read_reg(0x000, &foo);
     printf("foo = 0x%x\n", foo);
 
-    /*stk1160_set_videosource(STK1160_VIDEO_SOURCE_COMPOSITE1);
-    stk1160_read_reg(0x000, &foo);
-    printf("foo = 0x%x\n", foo);
-
-    stk1160_set_videosource(STK1160_VIDEO_SOURCE_COMPOSITE0);
-    stk1160_read_reg(0x000, &foo);
-    printf("foo = 0x%x\n", foo);
-
-    stk1160_set_videosource(STK1160_VIDEO_SOURCE_COMPOSITE2);
-    stk1160_read_reg(0x000, &foo);
-    printf("foo = 0x%x\n", foo);*/
+    buffer = malloc(BUFFER_SIZE);
+    buf_p = buffer;
+    frame_counter = 0;
+    packet_counter = 0;
 
     int ret;
-    ret = stk1160_start_streaming();
+    ret = stk1160_start_streaming(our_handler);
     printf("ret = %d\n", ret);
 
     return 0;
 }
-#endif
+
+void our_handler(uint8_t *data, uint16_t length)
+{
+    printf("our_handler(%p, %d)\n", data, length);
+
+    if (data[0] == 0xc0)
+    {
+        printf(">>> START OF NEW FRAME (%d) %d\n", ++frame_counter, packet_counter);
+
+        // DO SOME PROCESSING HERE FOR WE *SHOULD* HAVE A COMPLETE FRAME
+        char* filename = malloc(32);
+        snprintf(filename, 32, "/tmp/stk1160/frame-%04d", frame_counter);
+        FILE* file = fopen(filename, "w");
+        int written = fwrite(buffer, sizeof(uint8_t), buf_p - buffer, file);
+        printf("wrote %d bytes\n", written);
+        fclose(file);
+
+        buf_p = buffer;
+    }
+
+    if (buf_p + length < buffer + BUFFER_SIZE)
+    {
+        memcpy(buf_p, data, length);
+        buf_p += length;
+        printf("got %d bytes of \"video\"-data\n", buf_p - buffer);
+        packet_counter++;
+    }
+
+    else
+    {
+        buf_p = buffer;
+        printf("our_handler: ran out of memory, skip this frame\n");
+    }
+}
